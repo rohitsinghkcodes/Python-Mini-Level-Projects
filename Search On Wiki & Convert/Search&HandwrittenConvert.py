e8a1c32e817a9adc8a8e76104dc3b1828e9227c7
Rohit Singh <conankmails@gmail.com>
2020-12-20 18:31:55 +0530

    successfully completed beta testing for search and conversion

--- Search On Wiki & Convert/Search&HandwrittenConvert.py
+++ Search On Wiki & Convert/Search&HandwrittenConvert.py
@@ -4,16 +4,17 @@ import wikipedia
 
 #searching on wikipedia and returning the result
 def wikiSearch(search):
-    return wikipedia.summary(search,sentences = 3)
+    return wikipedia.summary(search,sentences = 10)
 
 #converting into hand written form
 def handWritten(searchElement):
     text = wikiSearch(searchElement)
-    pywhatkit.text_to_handwriting(text,rgb=[0,0,0])
+    pywhatkit.text_to_handwriting(text,rgb=[0,0,0],save_to='converted.png')
 
 
 if __name__ == "__main__":
-    handWritten('India')
+    searchElement = input('WHAT DO YOU WANT TO SEARCH AND CONVERT : ')
+    handWritten(searchElement)
 
 
 
