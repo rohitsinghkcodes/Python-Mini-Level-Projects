cc557be41eece0d9d1eae8562829dbcc2d2d5ac1
Rohit Singh <conankmails@gmail.com>
2020-12-20 18:25:03 +0530

    search and conversion completed

--- Search&HandwrittenConvert.py
+++ Search&HandwrittenConvert.py
@@ -1,13 +1,19 @@
 # importing modules
 import pywhatkit
+import wikipedia
 
 #searching on wikipedia and returning the result
 def wikiSearch(search):
-    return pywhatkit.
+    return wikipedia.summary(search,sentences = 3)
+
+#converting into hand written form
+def handWritten(searchElement):
+    text = wikiSearch(searchElement)
+    pywhatkit.text_to_handwriting(text,rgb=[0,0,0])
 
 
 if __name__ == "__main__":
-    pass
+    handWritten('India')
 
 
 
