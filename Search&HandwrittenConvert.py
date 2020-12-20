4d0226143bc4ebf26907d9ca1b350f77939092c8
Rohit Singh <conankmails@gmail.com>
2020-12-20 18:08:07 +0530

    Search and handwritten from conversion project started

---
+++ Search&HandwrittenConvert.py
@@ -0,0 +1,13 @@
+# importing modules
+import pywhatkit
+
+#searching on wikipedia and returning the result
+def wikiSearch(search):
+    return pywhatkit.
+
+
+if __name__ == "__main__":
+    pass
+
+
+
